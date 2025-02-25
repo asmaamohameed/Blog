6ac37cf4b7b58520041f76810162ae9ea5ca451d
asmaamohameed <asmaa.mo104@gmail.com>
2025-02-25 17:58:43 +0200

    BLOG-01: Feature(Project sturcture) - (Added)

---
+++ src/Views/Home.php
@@ -0,0 +1,12 @@
+<?php require_once 'partials/Navbar.php'?>
+
+<div class="relative isolate overflow-hidden bg-gray-900 h-screen py-24 sm:py-32">
+  <img src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply" alt="" class="absolute inset-0 -z-10 size-full object-cover object-right md:object-center">
+  <div class="mx-auto max-w-2xl px-2 lg:px-8">
+    <div class="mx-auto max-w-2xl lg:mx-0 text-center ">
+      <h2 class="text-5xl font-semibold tracking-tight text-white sm:text-7xl">Home Page</h2>
+      <p class="mt-8 text-lg font-medium text-pretty text-gray-300 sm:text-xl/8">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat.</p>
+    </div>
+  </div>
+</div> 
+
