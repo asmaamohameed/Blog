af088acf4eaa19e5a30b882041bfe163e6cf221a
asmaamohameed <asmaa.mo104@gmail.com>
2025-03-14 02:04:36 +0200

    BLOG-06: Feature(Auth, Validation) - (add login and register forms, add validation)

--- public/index.php
+++ public/index.php
@@ -5,15 +5,14 @@ require_once '../vendor/autoload.php';
 require_once '../app/Routes/web.php';
 
 use Blog\Application;
-use Blog\Http\Route;
-use Blog\Database\Database;
+use Blog\Validation\Validator;
 
 $app = new Application();
 
 $app->run();
 
-// $db = Database::getInstance()->getConnection();
 
 
 
-// dd($db->query('SELECT * FROM articals')->fetchAll());
+    
+
---
+++ src/Validation/Rules/ConfirmedRul.php
@@ -0,0 +1,17 @@
+<?php 
+
+namespace Blog\Validation\Rules;
+
+use Blog\Validation\Rule;
+
+// class ConfirmedRule implements Rule
+// {
+//     public function apply($field, $value, $data = null): bool
+//     {
+        
+//     }
+//     public function message(): string
+//     {
+//         return "The :attribute does not match the confirmation.";
+//     }
+// }
